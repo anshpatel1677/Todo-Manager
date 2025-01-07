56366296a1cfd2d039b8983e8b84faa5a3119299
anshpatel1677 <anshpatel1677@gmail.com>
2025-01-07 11:35:38 +0530

    configration issue fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: process.env.PORT ? parseInt(process.env.PORT) : 10000, // Use Render's specified port
+  },
 })
